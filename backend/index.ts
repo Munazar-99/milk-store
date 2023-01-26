9bafc32e383f6a7d175b9bbce39a4233e377d3b4
Munazar Abdulle <munazar.ali.001@gmail.com>
2023-01-26 12:37:12 +0100

    deploys backend

--- backend/index.ts
+++ backend/index.ts
@@ -5,7 +5,7 @@ import { data } from './data/database';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
   res.json(data);
