67effa868d92c27662f5628dc9fe040cf16900c6
Munazar Abdulle <munazar.ali.001@gmail.com>
2023-01-27 17:41:46 +0100

    adds loading animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,34 +2,43 @@ import React, { useEffect, useState } from 'react';
 import 'antd/dist/reset.css';
 import './App.css';
 import MilkBoard from './componennts/MilkBoard';
-import {Input} from 'antd'
+import { Input, Space, Spin } from 'antd'
 import Filter from './componennts/Filter';
 import { Model } from './model'
 
 function App() {
   const [data, setData] = useState<Model>()
   const [page, setPage] = useState<number>(1)
+  const [loading, setLoading] = useState<boolean>(true)
   const [filteredList, setFilteredList] = useState<string[]>([])
   useEffect(() => {
-    if(typeof(process.env.REACT_APP_URL) === 'string') {
+    if (typeof (process.env.REACT_APP_URL) === 'string') {
       fetch(`${process.env.REACT_APP_URL}`)
-      .then(response => response.json())
-      .then(res => setData(res) )
-      .catch(err => console.log(err.message))
+        .then(response => response.json())
+        .then(res => setData(res))
+        .catch(err => console.log(err.message))
+        .finally(() => setLoading(false))
     }
   }, [])
   return (
     <>
-    <header className='heading'>
-      <h2 className='heading-title'>THE MILK STORE</h2>
-    </header>
-    <main className='main-display'>
-      <section className='utilities'>
-      <Input.Search   type="text" placeholder='Search' onSearch={() => console.log('heeeey')} className='search-field' />
-      < Filter setPage={setPage}   setFilteredList={setFilteredList} />
-      </section>
-      {data && <MilkBoard page={page} setPage={setPage} data={data} filteredList={filteredList} />}
-    </main>
+      <header className='heading'>
+        <h2 className='heading-title'>THE MILK STORE</h2>
+      </header>
+      <main className='main-display'>
+        <section className='utilities'>
+          <Input.Search type="text" placeholder='Search' onSearch={() => console.log('heeeey')} className='search-field' />
+          < Filter setPage={setPage} setFilteredList={setFilteredList} />
+        </section>
+        {data && <MilkBoard page={page} setPage={setPage} data={data} filteredList={filteredList} />}
+        {loading &&
+          <section className='spinner-container'>
+            <Space align='center' size="large">
+              <Spin wrapperClassName='spinner' tip='Loading...' size="large" />
+            </Space>
+          </section>
+        }
+      </main>
     </>
   );
 }
